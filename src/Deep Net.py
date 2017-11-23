250c012632731b6b057733b2b21de84f73a741b0
Eric Bolboa <ebolboa@gmail.com>
2017-11-23 04:18:50 -0500

    added a plot for the Linear Regression model in Tensorflow

--- src/Deep Net.py
+++ src/Deep Net.py
@@ -16,13 +16,13 @@ flags.DEFINE_integer('display_step', 2, 'Tells function to print out progress af
 def DNN(target, data):
 
 	# Drop the target label, which we save separately.
-    X = np.array(data.drop([target], axis=1).values)
-    Y = np.array(data[target].values)
-    Y = np.reshape(Y, (-1, 1))
+    X_data = np.array(data.drop([target], axis=1).values)
+    Y_data = np.array(data[target].values)
+    Y_data = np.reshape(Y_data, (-1, 1))
 
     X_train, X_test, Y_train, Y_test = train_test_split(
-    	X, 
-    	Y, 
+    	X_data, 
+    	Y_data, 
     	test_size=0.2
     )
 
@@ -71,18 +71,33 @@ def DNN(target, data):
 
     	print("Optimization Finished!")
 
-    	training_cost = sess.run(cost, feed_dict={X:X_train, Y:Y_train})
-    	print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+    	pred_y = sess.run(pred, feed_dict={X:X_test})
+    	mse = tf.reduce_mean(tf.square(pred_y - Y_test))
 
-    	testing_cost = sess.run(
-    		tf.reduce_sum(tf.pow(pred-Y, 2)) / (2*X_test.shape[0]),
-    		feed_dict={X:X_test, Y:Y_test}
-    	)
+    	print("MSE: %4f" % sess.run(mse))
 
-    	print("Testing cost=", testing_cost)
-    	print("Absolute mean square loss difference:", abs(
-    		training_cost - testing_cost
-    	))
+
+    	input_values = X_data[:10]
+    	target_values = Y_data[:10]
+    	prediction_values = input_values * sess.run(W) + sess.run(b)
+
+    	plt.plot(input_values, target_values, 'ro', label='main')
+    	plt.plot(input_values, prediction_values, label='Predicted')
+
+    	plt.show()
+
+    	# training_cost = sess.run(cost, feed_dict={X:X_train, Y:Y_train})
+    	# print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+
+    	# testing_cost = sess.run(
+    	# 	tf.reduce_sum(tf.pow(pred-Y, 2)) / (2*X_test.shape[0]),
+    	# 	feed_dict={X:X_test, Y:Y_test}
+    	# )
+
+    	# print("Testing cost=", testing_cost)
+    	# print("Absolute mean square loss difference:", abs(
+    	# 	training_cost - testing_cost
+    	# ))
 
 
 def main(_):
